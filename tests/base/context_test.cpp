3e5417f9384ecff8cf17c79016785f3359cc40b3
Henrik Fransson <henrik.fransson@elekta.com>
2024-07-04 17:33:24 +0200

    Add simple test with native zmq context

--- tests/base/context_test.cpp
+++ tests/base/context_test.cpp
@@ -12,3 +12,24 @@ SCENARIO("We compile and run the tests") {
     }
   }
 }
+
+SCENARIO("Using a native ZeroMQ context") {
+  GIVEN("a native zmq context") {
+    auto z_ctx = zmq_ctx_new();
+    REQUIRE(z_ctx != nullptr);
+
+    WHEN("Creating a zq context from the native") {
+      auto context = zq::mk_context(z_ctx);
+      THEN("we should have a valid zq context") {
+        CHECK(context);
+      }
+    }
+
+    WHEN("Creating a zq context from an invalid native context") {
+      auto context = zq::mk_context(nullptr);
+      THEN("we should not have a valid zq context") {
+        CHECK(!context);
+      }
+    }
+  }
+}
